528033da512b14c02ecd4203364c457f9aa23db2
KarthikeyaSarma30 <77091365+KarthikeyaSarma30@users.noreply.github.com>
2021-06-28 16:01:47 +0530

    Add files via upload

---
+++ listcomprehension.py
@@ -0,0 +1,10 @@
+str="python3.9.5"
+no=[i for i in str if i.isdigit()]
+print(no)
+str="Python3.9.5"
+upper=[i for i in str if i.isupper()]
+loweer=[j  for j in str if j.islower()]
+alpha=[k for k in str if k.isalpha()]
+print(upper)
+print(loweer)
+print(alpha)
